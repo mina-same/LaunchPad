6b0e90b16a3a67133759f7a4eec2dec31d125208
mina samy lamhy <m.lamhy02598@dmu.edu.eg>
2024-01-21 21:13:30 +0200

    first commit

--- app/index.js
+++ app/index.js
@@ -1,11 +1,45 @@
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, SHADOWS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
-    return(
-        <View>
-            <Text>Home</Text>
+    const router = useRouter();
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+      <Stack.Screen
+        options={{
+            headerLeft: () => (
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+            ),
+            headerRight: () => (
+                <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+            ),
+            headerStyle:{ backgroundColor: COLORS.lightWhite },
+            headerShadowVisible: false,
+            headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{
+          flex: 1,
+          padding: SIZES.medium,
+        }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />  
         </View>
-    )
-}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,70 @@
+import React, {useState} from 'react'
+import { 
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  FlatList
+} from 'react-native'
+import { useRouter } from "expo-router";
+import styles from './welcome.style';
+import { SIZES, icons } from '../../../constants';
+
+const jopTypes= ["Full-Time", "part-Time", "internship", "freelance", "contractor"]
+
+const Welcome = () => {
+  const router = useRouter();
+  const [activeJopType, setActiveJopType] = useState("Full-Time")
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, Mina.</Text>
+        <Text style={styles.welcomeMessage}>Find Your Perfect Jop.</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder='what are ou looking for?'
+            />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+            />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jopTypes}
+          renderItem={({item}) =>(
+            <TouchableOpacity
+              style={styles.tab(activeJopType, item)}
+              onPress={() => { 
+                setActiveJopType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJopType,item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+
+    </View>
+  )
+}
+
+export default Welcome
